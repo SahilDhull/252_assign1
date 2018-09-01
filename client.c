5828aeff9a20f92352d447e600418453297dbb99
Siddhant Sarkar <siddhantsarkar2404@gmail.com>
2018-09-01 16:46:56 +0530

    Recieving multiple files

--- client.c
+++ client.c
@@ -4,15 +4,48 @@
 #include<arpa/inet.h>
 #include<sys/ioctl.h>
 #include<unistd.h>
-// #include<iostream>
+#include<stdlib.h>
 // #include<fstream>
 #include<errno.h>
-// using namespace std;
+//using namespace std;
 
 //This function is to be used once we have confirmed that an image is to be sent
 //It should read and output an image file
 
 char count[5] = {'0','0','0','0','\0'};
+char name[100];
+
+void reverse(char *x, int begin, int end)
+{
+   char c;
+
+   if (begin >= end)
+      return;
+
+   c          = *(x+begin);
+   *(x+begin) = *(x+end);
+   *(x+end)   = c;
+
+   reverse(x, ++begin, --end);
+}
+
+
+void make_name(int i)
+{
+  int j = 0;
+  while(i > 0)
+  {
+    name[j++] = (i%10) + 48;
+    i/=10;
+  }
+  name[j] = '\0';
+  reverse(name,0,j-1);
+  char ext[6] = ".jpeg";
+  //itoa(i,name,3);
+  strcat(name,ext);
+
+}
+
 
 void parse(char s[])
 {
@@ -50,7 +83,7 @@ void parse(char s[])
 
 
 
-int receive_image(int socket)
+int receive_image(int socket, char name[])
 { // Start function
 
     int buffersize = 0, recv_size = 0,size = 0, read_size, write_size, packet_index =1,stat;
@@ -78,7 +111,7 @@ int receive_image(int socket)
     printf("Reply sent\n");
     printf(" \n");
 
-    image = fopen("capture2.jpeg", "w");
+    image = fopen(name, "w");
 
     if( image == NULL) {
         printf("Error has occurred. Image file could not be opened\n");
@@ -138,7 +171,7 @@ int receive_image(int socket)
 
 
     fclose(image);
-    printf("Image successfully Received!\n");
+    printf("Image %s successfully Received!\n",name);
     return 1;
 }
 
@@ -149,6 +182,7 @@ int main(int argc , char *argv[])
     struct sockaddr_in server;
     char *parray;
 
+    char query[100];
 
     //Create socket
     socket_desc = socket(AF_INET , SOCK_STREAM , 0);
@@ -172,7 +206,24 @@ int main(int argc , char *argv[])
 
     puts("Connected\n");
 
-    receive_image(socket_desc);
+    printf("Enter Query: ");
+    scanf("%s", query);
+    parse(query);
+
+    int n;
+
+    for(int i = 0 ; i < 4 ; i++)
+    {
+      n+=(count[i] - '0');
+    }
+
+
+
+    for(int i = 0 ; i < n ; i++)
+    {
+      make_name(i);
+      receive_image(socket_desc,name);
+    }
 
     close(socket_desc);
 
