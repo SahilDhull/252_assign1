f0fc29ef3a9da6993edb0a5cb04c78fb6092f7f4
shubhuiitk <shubhuiitk@gmail.com>
2018-09-01 16:06:05 +0530

    parsing added and indentation fixed

--- client.c
+++ client.c
@@ -12,51 +12,90 @@
 //This function is to be used once we have confirmed that an image is to be sent
 //It should read and output an image file
 
+char count[5] = {'0','0','0','0','\0'};
+
+void parse(char s[])
+{
+    int i=0;
+    int dogs=0,cats=1,cars=2,trucks=3;
+    char d[]="dog";
+    char ds[]="dogs";
+    char c[]="car";
+    char cs[]="cars";
+    char ct[]="cat";
+    char cts[]="cats";
+    char t[]="truck";
+    char ts[]="trucks";
+    while(s[i]!='\0')
+    {
+        while(s[i]>'4' || s[i]<'1') i++;
+        char n = s[i];
+        i++;
+        while(s[i]<'a' || s[i]>'z') i++;
+        char temp[10];
+        int j=0;
+        while(s[i]>='a' && s[i]<='z')
+        {
+            temp[j]=s[i];
+            i++;
+            j++;
+        }
+        temp[j]='\0';
+        if(!strcmp(d,temp) || !strcmp(ds,temp)) count[dogs] = n;
+        else if (!strcmp(c,temp) || !strcmp(cs,temp)) count[cars] = n;
+        else if (!strcmp(ct,temp) || !strcmp(cts,temp)) count[cats] = n;
+        else if (!strcmp(t,temp) || !strcmp(ts,temp)) count[trucks] = n;
+    }
+}
+
+
+
 int receive_image(int socket)
 { // Start function
 
-int buffersize = 0, recv_size = 0,size = 0, read_size, write_size, packet_index =1,stat;
+    int buffersize = 0, recv_size = 0,size = 0, read_size, write_size, packet_index =1,stat;
 
-char imagearray[10241],verify = '1';
-FILE *image;
+    char imagearray[10241],verify = '1';
+    FILE *image;
 
-//Find the size of the image
-do{
-stat = read(socket, &size, sizeof(int));
-}while(stat<0);
+    //Find the size of the image
+    do{
+        stat = read(socket, &size, sizeof(int));
+    }while(stat<0);
 
-printf("Packet received.\n");
-printf("Packet size: %i\n",stat);
-printf("Image size: %i\n",size);
-printf(" \n");
+    printf("Packet received.\n");
+    printf("Packet size: %i\n",stat);
+    printf("Image size: %i\n",size);
+    printf(" \n");
 
-char buffer[] = "Got it";
+    char buffer[] = "Got it";
 
-//Send our verification signal
-do{
-stat = write(socket, &buffer, sizeof(int));
-}while(stat<0);
+    //Send our verification signal
+    do{
+        stat = write(socket, &buffer, sizeof(int));
+    }while(stat<0);
 
-printf("Reply sent\n");
-printf(" \n");
+    printf("Reply sent\n");
+    printf(" \n");
 
-image = fopen("capture2.jpeg", "w");
+    image = fopen("capture2.jpeg", "w");
 
-if( image == NULL) {
-printf("Error has occurred. Image file could not be opened\n");
-return -1; }
+    if( image == NULL) {
+        printf("Error has occurred. Image file could not be opened\n");
+        return -1;
+    }
 
-//Loop while we have not received the entire file yet
+        //Loop while we have not received the entire file yet
 
 
-int need_exit = 0;
-struct timeval timeout = {10,0};
+    int need_exit = 0;
+    struct timeval timeout = {10,0};
 
-fd_set fds;
-int buffer_fd, buffer_out;
+    fd_set fds;
+    int buffer_fd, buffer_out;
 
-while(recv_size < size) {
-//while(packet_index < 2){
+    while(recv_size < size) {
+        //while(packet_index < 2){
 
     FD_ZERO(&fds);
     FD_SET(socket,&fds);
@@ -64,78 +103,78 @@ while(recv_size < size) {
     buffer_fd = select(FD_SETSIZE,&fds,NULL,NULL,&timeout);
 
     if (buffer_fd < 0)
-       printf("error: bad file descriptor set.\n");
+    printf("error: bad file descriptor set.\n");
 
     if (buffer_fd == 0)
-       printf("error: buffer read timeout expired.\n");
+    printf("error: buffer read timeout expired.\n");
 
     if (buffer_fd > 0)
     {
         do{
-               read_size = read(socket,imagearray, 10241);
-            }while(read_size <0);
+            read_size = read(socket,imagearray, 10241);
+        }while(read_size <0);
 
-            printf("Packet number received: %i\n",packet_index);
+        printf("Packet number received: %i\n",packet_index);
         printf("Packet size: %i\n",read_size);
 
 
         //Write the currently read data into our image file
-         write_size = fwrite(imagearray,1,read_size, image);
-         printf("Written image size: %i\n",write_size);
+        write_size = fwrite(imagearray,1,read_size, image);
+        printf("Written image size: %i\n",write_size);
 
-             if(read_size !=write_size) {
-                 printf("error in read write\n");    }
+        if(read_size !=write_size) {
+            printf("error in read write\n");    }
 
 
-             //Increment the total number of bytes read
-             recv_size += read_size;
-             packet_index++;
-             printf("Total received image size: %i\n",recv_size);
-             printf(" \n");
-             printf(" \n");
+            //Increment the total number of bytes read
+            recv_size += read_size;
+            packet_index++;
+            printf("Total received image size: %i\n",recv_size);
+            printf(" \n");
+            printf(" \n");
+        }
+
     }
 
+
+    fclose(image);
+    printf("Image successfully Received!\n");
+    return 1;
 }
 
+int main(int argc , char *argv[])
+{
 
-  fclose(image);
-  printf("Image successfully Received!\n");
-  return 1;
-  }
-
-  int main(int argc , char *argv[])
-  {
-
-  int socket_desc;
-  struct sockaddr_in server;
-  char *parray;
+    int socket_desc;
+    struct sockaddr_in server;
+    char *parray;
 
 
-  //Create socket
-  socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    //Create socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
 
-  if (socket_desc == -1) {
-  printf("Could not create socket");
-  }
+    if (socket_desc == -1) {
+        printf("Could not create socket");
+    }
 
-  memset(&server,0,sizeof(server));
-  server.sin_addr.s_addr = inet_addr("127.0.0.1");
-  server.sin_family = AF_INET;
-  server.sin_port = htons( 5000 );
+    memset(&server,0,sizeof(server));
+    server.sin_addr.s_addr = inet_addr("127.0.0.1");
+    server.sin_family = AF_INET;
+    server.sin_port = htons( 5000 );
 
-  //Connect to remote server
-  if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0) {
-  // cout<<strerror(errno);
-  close(socket_desc);
-  puts("Connect Error");
-  return 1;
-  }
+    //Connect to remote server
+    if (connect(socket_desc , (struct sockaddr *)&server , sizeof(server)) < 0) {
+        // cout<<strerror(errno);
+        close(socket_desc);
+        puts("Connect Error");
+        return 1;
+    }
 
-  puts("Connected\n");
+    puts("Connected\n");
 
-  receive_image(socket_desc);
+    receive_image(socket_desc);
 
-  close(socket_desc);
+    close(socket_desc);
 
-  return 0;
-  }
+    return 0;
+}
