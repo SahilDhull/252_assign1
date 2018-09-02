ffb7c5ebc728a98b5cc8ec3a88570d099896083c
lyadzii <lyazii327@gmail.com>
2018-09-02 20:41:01 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -10,9 +10,27 @@ echo $s>>index.html
 
 counter=1
 
-if [ $2 -gt 0 ]
+if [ $1 -gt 0 ]
 then
     echo '<font size="6"><b>DOGS</b> <br></font>' >>index.html
+    echo '<table style="width:100%">'>>index.html
+    echo '<tr>'>>index.html
+    i=1
+    while [ $i -le $1 ]
+    do
+        echo '<td>'>>index.html
+        echo '<img src="'"$counter"'.jpg" width="400" height="300"/>' >>index.html
+        echo '</td>'>>index.html
+        ((counter++))
+        ((i++))
+    done
+    echo '</tr>'>>index.html
+    echo '</table>'>>index.html
+fi
+
+if [ $2 -gt 0 ]
+then
+    echo '<font size="6"><b>CATS</b> <br></font>' >>index.html
     echo '<table style="width:100%">'>>index.html
     echo '<tr>'>>index.html
     i=1
@@ -30,7 +48,7 @@ fi
 
 if [ $3 -gt 0 ]
 then
-    echo '<font size="6"><b>CATS</b> <br></font>' >>index.html
+    echo '<font size="6"><b>CARS</b> <br></font>' >>index.html
     echo '<table style="width:100%">'>>index.html
     echo '<tr>'>>index.html
     i=1
@@ -41,6 +59,7 @@ then
         echo '</td>'>>index.html
         ((counter++))
         ((i++))
+
     done
     echo '</tr>'>>index.html
     echo '</table>'>>index.html
@@ -48,30 +67,11 @@ fi
 
 if [ $4 -gt 0 ]
 then
-    echo '<font size="6"><b>CARS</b> <br></font>' >>index.html
-    echo '<table style="width:100%">'>>index.html
-    echo '<tr>'>>index.html
-    i=1
-    while [ $i -le $4 ]
-    do
-        echo '<td>'>>index.html
-        echo '<img src="'"$counter"'.jpg" width="400" height="300"/>' >>index.html
-        echo '</td>'>>index.html
-        ((counter++))
-        ((i++))
-
-    done
-    echo '</tr>'>>index.html
-    echo '</table>'>>index.html
-fi
-
-if [ $5 -gt 0 ]
-then
     echo '<font size="6"><b>TRUCKS</b> <br></font>' >>index.html
     echo '<table style="width:100%">'>>index.html
     echo '<tr>'>>index.html
     i=1
-    while [ $i -le $5 ]
+    while [ $i -le $4 ]
     do
         echo '<td>'>>index.html
         echo '<img src="'"$counter"'.jpg" width="400" height="300"/>' >>index.html
